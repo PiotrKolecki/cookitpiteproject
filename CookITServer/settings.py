e63c032844a0462d2b78133e7ce76447b8cb575b
PiotrKolecki <piotr.kolecki97@gmail.com>
2020-11-28 15:22:06 +0100

    static files change

--- CookITServer/settings.py
+++ CookITServer/settings.py
@@ -119,8 +119,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
-
 
 
 
@@ -130,10 +128,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
