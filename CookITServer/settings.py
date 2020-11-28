c52b956bb675132b72ae63c949c9ecc3526daaca
PiotrKolecki <piotr.kolecki97@gmail.com>
2020-11-28 15:29:31 +0100

    heroku static fix v2

--- CookITServer/settings.py
+++ CookITServer/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,10 +125,5 @@ USE_TZ = True
 
 # HEROKU
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+django_heroku.settings(locals())
 
