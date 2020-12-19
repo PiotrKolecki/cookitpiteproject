d4e0be13e1691a686729bcb8e79faa8702977edb
Pasik97 <k.pasik97@gmail.com>
2020-12-19 18:56:00 +0100

    created tests for index and account views

--- CookIT/tests.py
+++ CookIT/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse 
+from CookIT.views import index
+from django.test.client import RequestFactory
+from http.cookies import SimpleCookie
 
-# Create your tests here.
+class HomePageTest(SimpleTestCase):
+   def test_home_page(self):
+      response = self.client.get('/CookIT/')
+      self.assertEqual(response.status_code, 200)
+      self.assertTemplateUsed(response, 'base.html')
+      self.assertContains(response, 'Hello')
+
+class AccountPageTest(SimpleTestCase):
+   def test_user_with_cookie(self):
+      self.client.cookies = SimpleCookie({'userSession': 'testCookie'})
+      response = self.client.get('/CookIT/account/')
+      self.assertEqual(response.status_code, 200)
+      self.assertTemplateUsed(response, 'base.html')
+      self.assertContains(response, 'Account')
+
+   def test_user_without_cookie(self):
+      self.client.cookies = SimpleCookie({})
+      response = self.client.get('/CookIT/account/')
+      print(response)
+      self.assertEqual(response.status_code, 302)
+      self.assertEqual(response.url, '/CookIT/login')
